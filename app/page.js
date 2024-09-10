e7cae885867eef127532d0d8cd4aefe02b3f8c8d
umar7070 <mohdumar707080@gmail.com>
2024-09-10 22:40:01 +0530

    working prodcuts components

--- app/page.js
+++ app/page.js
@@ -1,24 +1,42 @@
-import React from "react";
+"use client";
+
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
+  const [product, setProduct] = useState([]);
+  console.log(product);
+  const getapi = async () => {
+    const res = await axios.get("https://dummyjson.com/products");
+    setProduct(res.data.products);
+  };
+  useEffect(() => {
+    getapi();
+  }, []);
   return (
     <>
       <div className="container ">
         <h1 className="font-bold my-10">Products</h1>
-        <div className="grid grid-cols-5  gap-10">
-          <div className=" w-[80%]">
-            <img src="/img/peproni.png" className="h-[150px] w-[100%] " />
-            <div className="text-center">
-              <h2 className=" font-bold  text-sm my-1">Havana spacal</h2>
-              <p className=" font-bold ">small</p>
-            </div>
-            <div className="flex justify-between p-1 my-1">
-              <p className="font-bold">$5</p>
-              <button className="bg-yellow-500 w-[50px] text-white rounded-full">
-                add
-              </button>
-            </div>
-          </div>
+        <div className="grid grid-cols-5  gap-5">
+          {product.map((curEle, index) => {
+            return (
+              <>
+                <div className=" w-[80%] border border-red-500 p-3">
+                  <img src={curEle.thumbnail} className="h-[150px] w-[100%] " />
+                  <div className="text-center">
+                    <h2 className=" font-bold  text-sm my-1">Havana spacal</h2>
+                    <p className=" font-bold ">small</p>
+                  </div>
+                  <div className="flex justify-between p-1 my-1">
+                    <p className="font-bold">$5</p>
+                    <button className="bg-yellow-500 w-[50px] text-white rounded-full hover:bg-green-700 hover:text-white">
+                      add
+                    </button>
+                  </div>
+                </div>
+              </>
+            );
+          })}
         </div>
       </div>
     </>
