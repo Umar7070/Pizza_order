7398557d0250f5aeaaf8f111626bcda42f7b048a
umar7070 <mohdumar707080@gmail.com>
2024-09-26 10:29:58 +0530

    working homecomponent

--- app/components/HomeProduct.jsx
+++ app/components/HomeProduct.jsx
@@ -1,33 +1,36 @@
 import React from "react";
-import FoodData from "@/constant/constant";
+import Cardsdata from "@/constant/constant";
 
 const HomeProduct = () => {
   return (
     <>
-      <div className="container grid grid-cols-5 pb-2 gap-5">
-        {FoodData.map((curEle, index) => {
+      <div className="container grid grid-cols-4 pb-2 gap-5">
+        {Cardsdata.map((curEle, index) => {
           return (
-            <>
-              <div className="w-[100%] shadow-lg rounded-lg p-3">
+            <div key={index}>
+              <div className="w-[100%] shadow-lg rounded-lg p-3 hover:shadow-2xl hover:scale-105 transition duration-300 ease-in-out">
                 <div className="flex justify-center items-center">
-                  <img src={curEle?.img} alt="" className="h-[150px]" />
+                  <img
+                    src={curEle?.imgdata}
+                    alt=""
+                    className="w-[100%] h-[200px] rounded-lg"
+                  />
                 </div>
-                <div className="flex justify-between  my-1">
-                  <p>onion pizz</p>
-                  <p>price</p>
+                <div className="flex justify-between  my-2">
+                  <p className="text-lg text-pink-600">{curEle.rname}</p>
+                  <p className="text-xl text-blue-700">₹{curEle.price}</p>
                 </div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Suscipit, id.
+                <p className="text-center font-bold my-3 text-gray-600">
+                 {curEle.address}
                 </p>
-                <div className="flex justify-between my-1">
-                  <p>cating</p>
+                <div className="flex justify-between items-center my-2">
+                  <img src={curEle.arrimg} alt="" className="h-5" />
                   <div className="bg-orange-400 rounded-full   px-2">
                     <button className="capitalize text-white">add</button>
                   </div>
                 </div>
               </div>
-            </>
+            </div>
           );
         })}
       </div>
--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ const page = () => {
     <>
       <div className="container my-10  flex justify-between items-center ">
         <div className="container flex flex-col gap-2">
-          <em className="text-2xl capitalize">are you hungry ?</em>
+          <em className="text-2xl capitalize text-red-400">are you hungry ?</em>
           <h1 className="text-4xl font-bold">Don't Wait !</h1>
           <div>
             <button className="bg-yellow-500  rounded-full text-white px-3 p-1 my-2 capitalize">
